3b934ff132272715351ba9fc8a5f440d959d28b8
PRANJAY GULERIA <pranjayguleria@gmail.com>
2023-05-08 10:27:28 +0530

    portfolio created

--- gitprofile-main/vite.config.js
+++ gitprofile-main/vite.config.js
@@ -6,14 +6,13 @@ import tailwindConfig from './tailwind.config.mjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // If you are deploying to https://<USERNAME>.github.io/, set base to '/'.
-    // If you are deploying to https://<USERNAME>.github.io/<REPO>/, for example your repository is at https://github.com/<USERNAME>/<REPO>, then set base to '/<REPO>/'.
-    base: '/gitprofile/',
-    plugins: [react()],
-    css: {
-        postcss: {
-            plugins: [tailwind(tailwindConfig), autoprefixer],
-            base: '/gitprofile-main',
-        },
+  // If you are deploying to https://<USERNAME>.github.io/, set base to '/'.
+  // If you are deploying to https://<USERNAME>.github.io/<REPO>/, for example your repository is at https://github.com/<USERNAME>/<REPO>, then set base to '/<REPO>/'.
+  base: '/gitprofile/',
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwind(tailwindConfig), autoprefixer],
     },
+  },
 });
